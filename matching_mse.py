a0187474751b02ad50c50aceb0d877a6aab53af7
nyzhhd <3268407065@qq.com>
2024-01-11 15:34:09 +0800

    梵蒂冈

--- matching_mse.py
+++ matching_mse.py
@@ -5,6 +5,22 @@ import time
 import matplotlib.pyplot as plt
 import os
 
+
+def mse(imageA, imageB):
+    '''
+    计算两个图像之间的规范化均方误差。
+    输入
+    imageA: 第一个图像
+    imageB: 第二个图像
+    输出
+    similarity: 规范化的均方误差，作为相似度度量，范围在0到1之间。
+    '''
+    err = np.sum((imageA.astype("float") - imageB.astype("float")) ** 2)
+    err /= float(imageA.shape[0] * imageA.shape[1])
+    max_err = 255.0 ** 2
+    similarity = 1 - (err / max_err)
+    return similarity
+
 def find_defects_by_comparison_sift(original_image,template_image):
     '''
     输入
@@ -59,43 +75,42 @@ def find_defects_by_comparison_sift(original_image,template_image):
     # gray_image2 = cv2.equalizeHist(gray_image2)
 
     # 计算相似性度量
-
-    ssim = compare_ssim(gray_image1, gray_image2)  #------------------------------------- 利用compare_ssim函数比较两个图的结构相似度
+    mse_sim= mse(gray_image1, gray_image2)  #------------------------------------- 利用mse函数比较两个图的结构相似度
 
     # 显示结果图像
     # cv2.imshow("Matched Image Sift", matched_image)
     # cv2.waitKey(0)
     # cv2.destroyAllWindows()
-    return ssim
+    return mse_sim
 
 def find_defects_by_comparison_Bolt(original_image,threshold):
         have_defect = False
-        gk_norm_folder = 'gk_mb_20231113'   #-----------------------------------------------#模板库 ： 存放正常的模板
-        simm_list=[]
+        gk_norm_folder = 'gk_mb_20231113'        #-----------------------------------------------#模板库 ： 存放正常的模板
+        mse_list=[]
         for filename in os.listdir(gk_norm_folder):
             image_norm_path = os.path.join(gk_norm_folder, filename)
             template_image = cv2.imread(image_norm_path)
             try:
-               simm = find_defects_by_comparison_sift(original_image, template_image)
+               mse_sim = find_defects_by_comparison_sift(original_image, template_image)
             except:
-                simm=0
-            simm_list.append(simm)
-        simm_avg = sum(simm_list) / len(simm_list)
-        simm_max = max(simm_list)
-        print("最大相似度：",simm_max)
-        print("平均相似度：",simm_avg)
+                mse_sim =0
+            mse_list.append(mse_sim )
+        mse_avg = sum(mse_list) / len(mse_list)
+        mse_max = max(mse_list)
+        print("最大相似度：",mse_max)
+        print("平均相似度：",mse_avg)
 
-        if simm_avg>threshold:
+        if mse_avg>threshold:
             #print(f"{filename} 未检测到缺陷")
             have_defect=False
         else:
             #print(f"{filename} 检测到缺陷")
             have_defect=True
-        return have_defect,simm_avg
+        return have_defect,mse_avg
 
 
 if __name__ == "__main__":
-    threshold = 0.25
+    threshold = 0.522
     gk_folder = 'gk'              #----------------------------------------------------------------# 需要检测图像的文件夹
     defect_count = 0
     total_images = 0
@@ -106,11 +121,11 @@ if __name__ == "__main__":
         print(f'+++++++++++++++++{total_images}+++++++++++++++++++++++')
         image_path = os.path.join(gk_folder, filename)
         original_image = cv2.imread(image_path)
-        have_defect, simm = find_defects_by_comparison_Bolt(original_image, threshold)
+        have_defect, mse_sim = find_defects_by_comparison_Bolt(original_image, threshold)
         total_images += 1
 
         plt.imshow(cv2.cvtColor(original_image, cv2.COLOR_BGR2RGB))
-        plt.title(f"{filename} - {'have defect' if have_defect else 'no defect'} - {simm}")
+        plt.title(f"{filename} - {'have defect' if have_defect else 'no defect'} - {mse_sim}")
         plt.show()
 
         if have_defect:
